7a0a2d44c29ab700f393835a87cbbf4201950295
piyush0310 <piyushkumar8256@gmail.com>
2026-01-31 11:23:40 +0530

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,27 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import Heropage from '../components/Heropage';
 import MedicalTreatmentsPage from '../components/Treatments';
 import OurSpecialities from '../components/Specialities';
 import WhyChooseMuftMadad from '../components/WhyChoose';
 import PatientTestimonials from '../components/Testimonial';
-import MuftMadadBlogs from '../components/Blog';
 import Footer from '../components/Footer';
 import Navbar from '@/components/Navbar';
 
-
-
-const page = () => {
+const Page = () => {
   return (
     <div>
-      <Navbar />
+      <Suspense fallback={null}>
+        <Navbar />
+      </Suspense>
+
       <Heropage />
       <MedicalTreatmentsPage />
       <OurSpecialities />
       <WhyChooseMuftMadad />
       <PatientTestimonials />
-      {/* <MuftMadadBlogs /> */}
       <Footer />
     </div>
   );
-}
+};
 
-export default page;
+export default Page;
